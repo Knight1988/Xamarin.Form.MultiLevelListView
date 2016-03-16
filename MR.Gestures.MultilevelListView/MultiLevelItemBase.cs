fb452b5bac66a4a26aaeca8c392b089c7323f23e
Squall Leonhart <Knight1988@gmail.com>
2016-03-16 15:56:01 +0700

    update packages

---
+++ MR.Gestures.MultilevelListView/MultiLevelItemBase.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+
+namespace MR.Gestures
+{
+    public class MultiLevelItemBase
+    {
+        public List<MultiLevelItemBase> Children { get; set; } = new List<MultiLevelItemBase>();
+        public bool IsExpand { get; internal set; }
+        public string Name { get; set; }
+
+        public void Expand()
+        {
+            IsExpand = true;
+        }
+
+        public void Collapse()
+        {
+            IsExpand = false;
+            foreach (var child in Children)
+            {
+                child.Collapse();
+            }
+        }
+    }
+}
--- MultiLevelListview/MultiLevelListview.cs
+++ MultiLevelListview/MultiLevelListview.cs
@@ -52,6 +52,5 @@ namespace MultiLevelListview
                 item.Expand();
             }
         }
-
     }
 }
